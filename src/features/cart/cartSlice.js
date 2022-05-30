bbc19f67f460fe538411178ae466682af776a846
GG_NC <99155577+Noppawat3939@users.noreply.github.com>
2022-05-30 11:33:38 +0700

    Add files via upload

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,14 +1,22 @@
-import { createSlice } from "@reduxjs/toolkit";
-import cartItems from "../../cartItems";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+// import cartItems from "../../cartItems";
+
+const URL = 'https://course-api.com/react-useReducer-cart-project'
 
 //* create initialState
 
 const initialState = {
-    cartItems: cartItems,
+    cartItems: [],
     amount: 0,
     total: 0,
     isLoading: true,
 };
+
+//* createAsyncThunk
+
+export const getCartItems = createAsyncThunk('cart/getCartItems', () => {
+    return fetch(URL).then((res) => res.json()).catch((err) => console.log(err))
+})
 
 //* createSlice
 
@@ -42,6 +50,18 @@ const cartSlice = createSlice({
             })
             state.amount = amount;
             state.total = total.toFixed(2);
+        }
+    },
+    extraReducers: {
+        [getCartItems.pending]:(state) => {
+            state.isLoading = true
+        },
+        [getCartItems.fulfilled] : (state, action) => {
+            state.isLoading = false;
+            state.cartItems = action.payload;
+        },
+        [getCartItems.rejected]: (state) => {
+            state.isLoading = false;
         }
     }
 });
